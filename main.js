90072b866d86382a21a6467c42c6cf442dc90915
Damien van den IJssel <dovandenijssel@live.nl>
2024-09-20 18:28:34 +0200

    Got most of the layout done, now i need to get more cleaner code and fix JS bug

---
+++ main.js
@@ -0,0 +1,30 @@
+let a = 0
+let b = 0
+let c = 0
+
+function changeColor() {
+    let color = ["black", "white"];
+    let textColor = ["white", "black"];
+    let bColor = ["white", "black"];
+    document.getElementById("body").style.backgroundColor = color[a];
+    document.getElementById("body").style.color = textColor[b];
+    document.getElementById("body").style.border = bColor[c];
+    a++;
+    b++;
+    c++;
+    if (a == color.length) a = 0;
+    if(b == textColor.length) b = 0;
+    if(c ==bColor.length) c = 0;
+}
+
+
+
+// function changeTextColor() {
+//     let textColor = ["white", "black"];
+//     document.getElementById("body").style.color = textColor[b];
+//     b++;
+//     if (b == textColor.length) b = 0;
+// }
+
+
+
